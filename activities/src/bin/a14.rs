665866d0174fd7ddff5258ae31eb2b202b8b160c
Rhett Harrison <reharri7@asu.edu>
2022-10-05 07:02:03 -0700

    Advanced match and more enums topics

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,4 +11,45 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    age: i32,
+    name: String,
+    favorite_color: String
+}
+fn print_name(person: &Person) {
+    println!("{:?}", person.name);
+}
+fn print_favorite_color(person: &Person) {
+    println!("{:?}", person.favorite_color);
+}
+
+fn main() {
+    let people = vec![
+        Person {
+            name: String::from("Rhett"),
+            age: 24,
+            favorite_color: String::from("Red"),
+        },
+        Person {
+            name: String::from("Bailee"),
+            age: 22,
+            favorite_color: String::from("Purple"),
+        },
+        Person {
+            name: String::from("Bob"),
+            age: 10,
+            favorite_color: String::from("Green"),
+        },
+        Person {
+            name: String::from("Alice"),
+            age: 8,
+            favorite_color: String::from("Yellow"),
+        }
+    ];
+    for person in people {
+        if person.age <= 10 {
+            print_name(&person);
+            print_favorite_color(&person);
+        }
+    }
+}
--- activities/src/bin/a15.rs
+++ activities/src/bin/a15.rs
@@ -11,4 +11,23 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+enum Ticket {
+    Backstage(f64, String),
+    Standard(f64),
+    VIP(f64, String),
+}
+
+fn main() {
+    let tickets = vec![
+        Ticket::Backstage(50.0, "Billy".to_owned()),
+        Ticket::Standard(15.0),
+        Ticket::VIP(30.0, "Amy".to_owned()),
+    ];
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage(price, holder) => println!("Holder {:?}, price {:?}", holder, price),
+            Ticket::Standard(price) => println!("Price {:?}", price),
+            Ticket::VIP(price, holder) => println!("Holder {:?}, price {:?}", holder, price),
+        }
+    }
+}
